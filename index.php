031b81e49240ca50d09bce27043886d728d8193f
Higor Oliveira <83335750+DarkViPs@users.noreply.github.com>
2021-05-26 13:13:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Central Links</title>
+    <title>Link Center</title>
 
     <link rel="stylesheet" href="css/style.css">
 
@@ -12,9 +12,12 @@
 <body>
 <div class="card">
     <div class="card-top">
-         <h2 class="title">Central Links</h2>
-        <p>Aqui você encontrara todos os links de cada video do canal darkkk, tudo organizadinho.</p>
-        <p2 class="p2">Basta clicar no video que deseja, que vai aparecer os links.</p2>
+        <img class="logo" src="img/logo.png" alt="">
+         <h2 class="title">Link Center</h2>
+        <p>Aqui você tera acesso a download de varios conteudos criados por darkkk, principalmente, bots.</p2>
+            <div class="info">
+                <p2>O site é bem simples, basta clicar no conteudo desejado que ira aparecer os botões de download.</p2>
+                </div>
         </p2>
        </div>
        <div class="videos2"></div>
@@ -28,6 +31,12 @@
     <div class="videos2">
         <div class="imgs2">
         <img class="imgvideo2" src="img/video1.png" alt="">
+        <div class="botao1"> <a href="Videos-links/bot313.php"><button style="background: #ffffff; border-radius: 6px; padding: 15px; cursor: pointer; color: rgb(0, 0, 0); border: none; font-size: 16px;">ROBO(bot) PARA WHATSAPP COM 313 COMANDOS | BOT DE WHATSAPP PARA TERMUX</button></a></div>
+        </div>
+    </div>
+    <div class="videos3">
+        <div class="imgs3">
+        <img class="imgvideo3" src="img/video1.png" alt="">
         <div class="botao1"> <a href="Videos-links/bot313.php"><button style="background: #ffffff; border-radius: 6px; padding: 15px; cursor: pointer; color: rgb(0, 0, 0); border: none; font-size: 16px;">ROBO(bot) PARA WHATSAPP COM 313 COMANDOS | BOT DE WHATSAPP PARA TERMUX</button></a></div>
         </div>
     </div>
