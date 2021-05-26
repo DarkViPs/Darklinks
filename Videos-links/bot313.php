131d6edf466979cfa146dfb0bd3237dd19a91f64
Higor Oliveira <83335750+DarkViPs@users.noreply.github.com>
2021-05-26 12:39:39 -0300

    Add files via upload

---
+++ Videos-links/bot313.php
@@ -0,0 +1,173 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bot 313 Comandos</title>
+
+
+    <style>
+        body{
+    font-family: sans-serif;
+    background-color: #f0f0f0;
+}
+
+.form{
+ width: 400px;
+ margin: auto;
+ padding-top: 40px;
+}
+    .card{
+        box-shadow: 1px 1px 5px #ccc;
+        background-color: white;
+        padding: 30px;
+        border-radius: 5px;
+}
+
+.card-top{
+    text-align: center;
+}
+
+.card-group{
+    margin-bottom: 10px;
+}
+
+.card-groupB{
+    margin-bottom: 10px;
+    background-image: linear-gradient(to right, rgb(0, 132, 255), rgb(0, 10, 148));
+    border-radius: 30px;
+    width: 100%;
+    height: 40px;
+    padding: 5px;
+    color: white;
+    border: 0px;
+    outline: 0;
+}
+
+
+.card-group >button{
+    background-image: linear-gradient(to right, rgb(0, 132, 255), rgb(0, 10, 148));
+    border-radius: 30px;
+    width: 100%;
+    position: sticky;
+    height: 40px;
+    padding: 5px;
+    color: white;
+    border: 0px;
+    outline: 0;
+}
+
+.findiptitle {
+    width: 100%;
+}
+
+.api {
+    margin: auto;
+}
+
+.check{
+    padding: 5px;
+    color: yellowgreen;
+    width: 50px;
+}
+
+.form2{
+    padding: 5px;
+    color: rgb(55, 0, 255);
+}
+
+.imglogin{
+    width: 120px;
+    box-shadow: 1px 1px 5px #ccc;
+    border-radius: 80px;
+}
+
+.title{
+    color: orangered;
+}
+
+.card-group > label{
+    width: 100%;
+    color: rgb(98, 98, 98);
+    display: block;
+    
+}
+
+.card-group > input{
+   border-radius: 5px;
+   outline: 0;
+   width: 100%;
+   height: 30px;
+   padding: 5px;
+}
+
+.cmds{
+    position: relative;
+    left: 25px;
+top: 15px;
+}
+
+.arqv{
+
+position: relative;
+left: 25px;
+top: 15px;
+}
+
+.video{
+
+position: relative;
+left: 60px;
+top: 30px;
+width: 250px;
+}
+
+.zap{
+    position: relative;
+left: 1200px;
+top: 30px;
+width: 150px;   
+}
+    </style>
+
+</head>
+<body>
+    
+   <form class="form" action=https://suaurl.com/Zarchiver_dark method="POST">
+ <div class="card">
+     <div class="card-top">
+         <img class="imglogin" src="img/db.png" alt="">
+          <h2 class="title">Bot 313 Comandos</h2>
+         <p>Links do video:</p>
+        </div>
+<div class="card-group">
+    <button type="submit"> ZARCHIVER</button> 
+    <div class="card-group"></div>
+</form>
+    <form class="arqv" action=https://bit.ly/2SrAHMc method="POST">
+        <a href="#"><button style="background-image: linear-gradient(to right, rgb(0, 132, 255), rgb(0, 10, 148)); border-radius: 30px; width: 87%; position: sticky; height: 40px; padding: 5px; color: white; border: 0px; outline: 0;">ARQUIVO DO BOT</button></a>
+           <div class="card-group"></div></br>
+        </form>     
+<form class="cmds" action=https://bit.ly/3yyGWhM method="GET">
+    <a href="#"><button style="background-image: linear-gradient(to right, rgb(0, 132, 255), rgb(0, 10, 148)); border-radius: 30px; width: 87%; position: sticky; height: 40px; padding: 5px; color: white; border: 0px; outline: 0;">COMANDOS DO BOT</button></a>
+       <div class="card-group"></div></br>
+    </form>
+</form>     
+<form class="video" action=https://bit.ly/3yyGWhM method="GET">
+    <a href="#"><button style="background-image: linear-gradient(to right, rgb(255, 0, 0), rgb(255, 34, 34)); border-radius: 30px; width: 87%; position: sticky; height: 40px; padding: 5px; color: white; border: 0px; outline: 0;">Abrir o Video</button></a>
+       <div class="card-group"></div></br>
+    </form> 
+</form></br>
+</form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="zap">
+<a href="https://api.whatsapp.com/send?phone=5521999053515&text=Oi%20dark"><button style="background: #73ff00; border-radius: 6px; padding: 15px; cursor: pointer; color: #fff; border: none; font-size: 16px;">WhatsApp</button></a>
+</div>
+</body>
+</html>
